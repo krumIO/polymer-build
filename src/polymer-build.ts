a9180e6c1edd93767254b1360bab982213483cb8
Fred K. Schott <fschott@google.com>
2017-01-20 13:43:29 -0800

    pull html splitting/rejoining logic into new class, support controls

--- src/polymer-build.ts
+++ src/polymer-build.ts
@@ -3,6 +3,7 @@ export {SWConfig} from 'sw-precache';
 export {BuildAnalyzer, DepsIndex} from './analyzer';
 export {Bundler} from './bundle';
 export {forkStream} from './fork-stream';
+export {HtmlSplitter} from './html-splitter';
 export {PolymerProject} from './polymer-project';
 export {addServiceWorker, generateServiceWorker} from './service-worker';
 export {AddServiceWorkerOptions} from './service-worker';
